a43199bdac496ef569d32cb56f9d78987fca2a2c
Sanjeev Jha <sanjeev.jha@powerschool.com>
2025-12-25 12:53:04 +0530

    initial commit

---
+++ src/main/java/com/sanjeev/learnsping/LearnSpingApplication.java
@@ -0,0 +1,13 @@
+package com.sanjeev.learnsping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanjeev/learnsping/LearnSpingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjeev.learnsping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
